ef47626677d77560cdf32be9784faca69b4efd16
Daniel Hartl(Laptop) <daniel_hartl@gmx.at>
2016-02-06 10:00:28 +0100

    update deviceId

--- swe-iot/src/main/java/org/c02/iot/cloud/examples/DemoConstants.java
+++ swe-iot/src/main/java/org/c02/iot/cloud/examples/DemoConstants.java
@@ -1,7 +1,7 @@
 package org.c02.iot.cloud.examples;
 
 public interface DemoConstants {
-	String deviceId = "2e001f000547343232363230";
+	String deviceId = "36002e000747343337373738";
 	String accessToken = "2a9b72a4491967cb19d2ba10b07c40cc662dadf8";
 
 }
