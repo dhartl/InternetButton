3085d660b7beaedcbe9baa8a5a12988a674ab3a7
gfliess <gfliess@172.20.10.6>
2016-02-06 06:27:08 +0100

    initial

---
+++ swe-iot/src/main/java/org/c02/iot/cloud/examples/DemoConstants.java
@@ -0,0 +1,7 @@
+package org.c02.iot.cloud.examples;
+
+public interface DemoConstants {
+	String deviceId = "2e001f000547343232363230";
+	String accessToken = "2a9b72a4491967cb19d2ba10b07c40cc662dadf8";
+
+}
