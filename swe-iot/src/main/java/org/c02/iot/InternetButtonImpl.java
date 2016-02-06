7aed1d1c17f64cd8f3db69a6c04f01cfa0c6158d
philippgrob <philippg@outlook.at>
2016-02-06 11:26:32 +0100

    Korrektur Implementierung setLed

--- swe-iot/src/main/java/org/c02/iot/InternetButtonImpl.java
+++ swe-iot/src/main/java/org/c02/iot/InternetButtonImpl.java
@@ -18,28 +18,25 @@ public class InternetButtonImpl implements InternetButtonApi {
 	}
 
 	public void setLed(int postition, Color color) throws ParticleException {
-		
-		String temp; 
-		if(postition < 10)
-		{
-		temp = "0" + Integer.toString(postition);
+
+		String temp = "";
+		if (postition < 1 || postition > 12 || color == null) {
+			throw new IllegalArgumentException();
+
 		}
-		else
-		{
-			temp = Integer.toString(postition);
-		}
-		
-		String tempColor;
-		tempColor = Integer.toString(color.getRed()) + Integer.toString(color.getGreen()) +Integer.toString(color.getBlue());
-		
-		temp = temp + tempColor;
-		
-		wrapper.callMethod("led", temp);
-		
+
+		String parameter = integerTo3Chars(2, postition) + integerTo3Chars(3, color.getRed())
+				+ integerTo3Chars(3, color.getGreen()) + integerTo3Chars(3, color.getBlue());
+		wrapper.callMethod("led", parameter);
+
+	}
+
+	private String integerTo3Chars(int count, int color) {
+		return String.format("%0" + count + "d", color);
 	}
 
 	public void allLedsOff() throws ParticleException {
-		wrapper.callMethod("ledsOff",null);
+		wrapper.callMethod("ledsOff", null);
 
 	}
 
